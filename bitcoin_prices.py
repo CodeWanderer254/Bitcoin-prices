851eff109b4d2e2fde53db0667bde943cecc9952
CodeWanderer254 <161751497+CodeWanderer254@users.noreply.github.com>
2024-05-23 16:07:21 -0500

    Update bitcoin_prices.py

--- bitcoin_prices.py
+++ bitcoin_prices.py
@@ -7,29 +7,29 @@ from datetime import datetime
 url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
 
 
-# Functions to get Bitcoin price
+# Function to get Bitcoin price
 def get_bitcoin_price():
     try:
         response = requests.get(url)
         data = response.json()
-        price = data['bpi']['USD']['rate-float']
+        price = data['bpi']['USD']['rate_float']
         return price
     except Exception as e:
-        print(f"Error fetching data:) {e}")
+        print(f"Error fetching data: {e}")
         return None
-    
+
 
 # Function to write data to CSV
-def write_to_csv(timesstamp, price):
+def write_to_csv(timestamp, price):
     with open('bitcoin_prices.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([timesstamp,price])
+        writer.writerow([timestamp, price])
 
 
-#Main function to fetch price
+# Main function to fetch price
 def main():
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    price = get_bitcoin_price        
+    price = get_bitcoin_price()
     if price:
         write_to_csv(timestamp, price)
         print(f"Recorded at {timestamp}: ${price}")
@@ -38,10 +38,4 @@ def main():
 
 
 if __name__ == "__main__":
-  main()
-
-    
-
-
-
-
+    main()
